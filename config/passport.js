8f8de7a5ea7d4b00fd62b93cacc3b57470d3003b
Ankit Anurag <ankitvarun.mohanty@gmail.com>
2020-02-09 02:20:49 +0530

    pass

--- config/passport.js
+++ config/passport.js
@@ -58,8 +58,9 @@ module.exports = function(passport) {
             newUser.facebook.id = profile.id;
             newUser.facebook.token = accessToken;
             newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
-            newUser.facebook.email = profile.emails[0].value;
-
+            // newUser.facebook.email = profile.emails[0].value;
+            newUser.facebook.email = profile.email;
+            
             newUser.save(function(err){
               if(err)
                 throw err;
