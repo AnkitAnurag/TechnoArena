bdd7c106e8c5ecd4ef702a0c8c3abba48bf54295
Ankit Anurag <ankitvarun.mohanty@gmail.com>
2020-02-09 02:25:17 +0530

    as

--- models/User.js
+++ models/User.js
@@ -2,16 +2,16 @@ const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
   name: {
-    type: String,
-    required: true
+    type: String
+    // required: true
   },
   email: {
-    type: String,
-    required: true
+    type: String
+    // required: true
   },
   password: {
-    type: String,
-    required: true
+    type: String
+    // required: true
   },
   date: {
     type: Date,
