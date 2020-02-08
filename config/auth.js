2a8cd1cb95b6a420d655855abefcb4379604e599
Ankit Anurag <ankitvarun.mohanty@gmail.com>
2020-02-09 01:32:25 +0530

    as

--- config/auth.js
+++ config/auth.js
@@ -2,7 +2,7 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '187600225980212',
 		'clientSecret': '8e3a37d106a501fb5b2e5a84e0edb323',
-		'callbackURL': 'https://morning-woodland-26031.herokuapp.com/auth/facebook/callback'
+		'callbackURL': 'http://localhost:3000/auth/facebook/callback'
 	},
 
 	// 'googleAuth' : {
