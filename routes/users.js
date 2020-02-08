9bd731f972ca2ca113251503084450aa8b69e795
Ankit Anurag <ankitvarun.mohanty@gmail.com>
2020-02-09 02:17:16 +0530

    last

--- routes/users.js
+++ routes/users.js
@@ -84,7 +84,7 @@ router.post('/login', passport.authenticate('local',
 });
 
 
-router.get('/auth/facebook', passport.authenticate('facebook', {scope: ['email']}));
+router.get('/auth/facebook', passport.authenticate('facebook', {scope: 'email'}));
 
 router.get('/auth/facebook/callback', 
 	  passport.authenticate('facebook', { successRedirect: '/aboutus',
