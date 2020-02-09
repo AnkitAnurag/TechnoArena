350da47eb35b639938557222baa156f7944be33d
Ankit Anurag <ankitvarun.mohanty@gmail.com>
2020-02-10 00:22:50 +0530

    auth techno

--- config/auth.js
+++ config/auth.js
@@ -2,12 +2,12 @@ module.exports = {
 	'facebookAuth' : {
 		'clientID': '187600225980212',
 		'clientSecret': '8e3a37d106a501fb5b2e5a84e0edb323',
-		'callbackURL': 'https://morning-woodland-26031.herokuapp.com/auth/facebook/callback'
+		'callbackURL': 'https://technoarena.herokuapp.com/auth/facebook/callback'
 	},
 
 	'googleAuth' : {
 		'clientID': '1025068364951-lnk2pno0i0qp4e2qobj7d7h96u074kan.apps.googleusercontent.com',
 		'clientSecret': '7zDpEzx8LH4s5DNMcJV-TYSr',
-		'callbackURL': 'https://morning-woodland-26031.herokuapp.com/auth/google/callback'
+		'callbackURL': 'https://technoarena.herokuapp.com/auth/google/callback'
 	}
 }
