1091d7d16c75cf045f157aaf8446c099b3e851d0
Ankit Anurag <ankitvarun.mohanty@gmail.com>
2020-02-11 23:11:07 +0530

    deploy

--- models/User.js
+++ models/User.js
@@ -1,42 +1,42 @@
 const mongoose = require('mongoose');
 
 const UserSchema = new mongoose.Schema({
-  local: {
-    name: {
-    type: String
-    // required: true
-    },
-    email: {
-    type: String
-    // required: true
-    },
-    password: {
-    type: String
-    // required: true
-    },
-    date: {
-    type: Date,
-    default: Date.now
-    }
-  },
-  facebook: {
-		id: String,
-		token: String,
-		email: String,
-		name: String
-  },
+  // local: {
+  //   name: {
+  //   type: String
+  //   // required: true
+  //   },
+  //   email: {
+  //   type: String
+  //   // required: true
+  //   },
+  //   password: {
+  //   type: String
+  //   // required: true
+  //   },
+  //   date: {
+  //   type: Date,
+  //   default: Date.now
+  //   }
+  // },
+  // facebook: {
+	// 	id: String,
+	// 	token: String,
+	// 	email: String,
+	// 	name: String
+  // },
   google: {
 		id: String,
 		token: String,
 		email: String,
 		name: String
-  },
-  twitter: {
-		id: String,
-		token: String,
-		email: String,
-		name: String
-	}
+  }
+  // twitter: {
+	// 	id: String,
+	// 	token: String,
+	// 	email: String,
+	// 	name: String
+	// }
 });
 
 const User = mongoose.model('User', UserSchema);
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require("express");
 var router = express.Router();
 
+const Posts = require('../models/posts');
 
 //LANDING ROUTE
 router.get("/", function(req, res){
