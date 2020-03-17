a6e57447f7418d68e95be6bb23529ea900838800
Ankit Anurag <ankitvarun.mohanty@gmail.com>
2020-03-17 16:17:11 +0530

    login fix

--- models/User.js
+++ models/User.js
@@ -30,13 +30,13 @@ const UserSchema = new mongoose.Schema({
 		token: String,
 		email: String,
 		name: String
-  },
-  twitter: {
-		id: String,
-		token: String,
-		email: String,
-		name: String
-	}
+  }
+  // twitter: {
+	// 	id: String,
+	// 	token: String,
+	// 	email: String,
+	// 	name: String
+	// }
 });
 
 const User = mongoose.model('User', UserSchema);
