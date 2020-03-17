a1511cdbee6807e9987d8cfa0740f9e9c910fb0c
Ankit Anurag <ankitvarun.mohanty@gmail.com>
2020-03-17 18:15:48 +0530

    login fix

--- config/passport.js
+++ config/passport.js
@@ -60,7 +60,7 @@ module.exports = function(passport) {
             var newUser = new User();
             newUser.facebook.id = profile.id;
             newUser.facebook.token = accessToken;
-            newUser.facebook.name = profile.name.givenName + ' ' + profile.name.familyName;
+            newUser.facebook.name = profile.displayName;
             // newUser.facebook.email = profile.emails[0].value;
             newUser.facebook.email = profile.email;
             
