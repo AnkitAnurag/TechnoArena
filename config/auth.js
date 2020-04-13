4cc8b775f619c5137de32f62c5f40b8de89c6e37
Ankit Anurag <ankitvarun.mohanty@gmail.com>
2020-04-13 13:16:51 +0530

    .env added

--- config/auth.js
+++ config/auth.js
@@ -1,13 +1,13 @@
 module.exports = {
 	'facebookAuth' : {
-		'clientID': '187600225980212',
-		'clientSecret': '8e3a37d106a501fb5b2e5a84e0edb323',
-		'callbackURL': 'https://technoarena.herokuapp.com/auth/facebook/callback'
+		'clientID': process.env.FBCLIENTID,
+		'clientSecret': process.env.FBCLIENTSECRET,
+		'callbackURL': process.env.FBCALLBACKURL
 	},
 
 	'googleAuth' : {
-		'clientID': '1025068364951-lnk2pno0i0qp4e2qobj7d7h96u074kan.apps.googleusercontent.com',
-		'clientSecret': '7zDpEzx8LH4s5DNMcJV-TYSr',
-		'callbackURL': 'https://technoarena.herokuapp.com/auth/google/callback'
+		'clientID': process.env.GOOGLECLIENTID,
+		'clientSecret': process.env.GOOGLECLIENTSECRET,
+		'callbackURL': GOOGLECALLBACKURL
 	}
 }
